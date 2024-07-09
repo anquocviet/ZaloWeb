834047c8e1e6334b0fa86577d8d4c6330dd13c6e
VIE <anq.viet203@gmail.com>
2024-07-09 15:42:07 +0700

    Fix zegocloud

--- src/components/config/zegocloud.config.js
+++ src/components/config/zegocloud.config.js
@@ -1,26 +1,20 @@
-import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
-import { ZIM } from "zego-zim-web";
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import { ZIM } from 'zego-zim-web';
 
 let zegocloudConfig = (user) => {
-    const appID = 1851922968;
-    const serverSecret = "43324c1a6997f3e8dbcc5d74b3541a4c";
-    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-      appID,
-      serverSecret,
-      null,
-      user.id+"",
-      user.name
-    );
-    const zc = ZegoUIKitPrebuilt.create(kitToken);
-    zc.addPlugins({ ZIM });
-    zc.setCallInvitationConfig({
-        ringtoneConfig: {
-         incomingCallUrl: 'Nhac-chuong-cuoc-goi-Zalo.mp3',
-         outgoingCallUrl: 'nhac-chuong-rong-den-remix-tiktok.mp3' // The ringtone when sending a call invitation. 
-       }
-     })
+  const appID = 1356371488;
+  const serverSecret = '519036ed99423eb2cdfa59d496343d9f';
+  const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, null, user.id + '', user.name);
+  const zc = ZegoUIKitPrebuilt.create(kitToken);
+  zc.addPlugins({ ZIM });
+  zc.setCallInvitationConfig({
+    ringtoneConfig: {
+      incomingCallUrl: 'Nhac-chuong-cuoc-goi-Zalo.mp3',
+      outgoingCallUrl: 'nhac-chuong-rong-den-remix-tiktok.mp3', // The ringtone when sending a call invitation.
+    },
+  });
 
-    return zc
-}
+  return zc;
+};
 
-export default zegocloudConfig
+export default zegocloudConfig;
